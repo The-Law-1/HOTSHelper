196f0206e19c08ff6769c069e4e8f4850548aa43
Gabriel <gabriel.griffin@epitech.eu>
2022-03-17 11:38:13 +0100

    server - U: added a generic scraping function

--- server/src/heroes/heroes.controller.ts
+++ server/src/heroes/heroes.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Param, Query, Req } from "@nestjs/common";
-import { ApiQuery } from "@nestjs/swagger";
+import { Body, Controller, Get, Param, Post, Query, Req } from "@nestjs/common";
+import { ApiBody, ApiQuery } from "@nestjs/swagger";
 import { Hero } from "./hero.dto";
 import { HeroesService } from "./heroes.service";
 
@@ -37,4 +37,37 @@ export class HeroesController {
 
         return heroChoices;
     }
+
+    @Post("/team/synergies/:minSampleSize?/:selectionRange?")
+    // * need to do this so swagger still shows the param
+    @ApiQuery({
+        name: "minSampleSize",
+        type: Number,
+        description: "Minimum sample size, optional",
+        required: false,
+    })
+    @ApiQuery({
+        name: "selectionRange",
+        type: Number,
+        description: "Hero selection range, optional",
+        required: false,
+    })
+    @ApiBody({
+        type: [Hero],
+        description: "Allied heroes array"
+    })
+    async getTeamSynergies(
+        @Body() allyTeam : [Hero],
+        @Query("minSampleSize") minSampleSize? : number,
+        @Query("selectionRange") selectionRange? : number
+    ) : Promise<Array<Hero>>
+    {
+        // ! you really only need an array of names for the ally team
+        let heroChoices = await this.heroesService.getSynergyWinrates(
+            allyTeam, minSampleSize, selectionRange
+        )
+
+        return (heroChoices);
+    }
+
 }
--- server/src/heroes/heroes.service.ts
+++ server/src/heroes/heroes.service.ts
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 import { Injectable } from "@nestjs/common";
-import { NOTFOUND } from "dns";
+import { Browser, Page } from "puppeteer";
+import { sample } from "rxjs";
 import { Hero } from "./hero.dto";
 
 enum Sorting {
@@ -35,12 +36,97 @@ export class HeroesService {
         minSampleSize: number = 30,
         selectionRange: number = 8
     ): Promise<Array<Hero>> {
-        let allHeroStats = [];
         let heroChoices = [];
 
-        // todo copy hero choices, get average, filter
+        let bestChoicesArray = []; // * array of arrays of best heroes for each ally member
+        let worstChoicesArray = []; // * array of arrays of worst heroes for each ally member
+
+        const browser = await puppeteer.launch({ headless: false });
+
+        // ! test and fix, the returned array is empty
+        // * export this to function
+            let allHeroStats = [];
+            const page = await browser.newPage();
+            // ! hero name needs to be properly written and capital letters at the start of words
+            await page.goto(
+                `https://www.hotslogs.com/Sitewide/TalentDetails?Hero=${allyTeam[0].name}&Tab=winRateWithOtherHeroes`
+            );
+            allHeroStats = await this.scrapeGenericTable(minSampleSize, page);
+            console.log(allHeroStats);
+
+            bestChoicesArray = this.filterHeroesWinrate(allHeroStats, Sorting.Ascending, selectionRange);
+            worstChoicesArray = this.filterHeroesWinrate(allHeroStats, Sorting.Descending, selectionRange);
+
+        // * export this to function
+
+
+        // * for each allied hero, push synergies to best/worst choices array
+
+        // * loop over arrays, highest average remains in final heroChoices
+        heroChoices = heroChoices.concat(bestChoicesArray, worstChoicesArray);
+
+        console.log("Closing browser");
+        await browser.close();
 
         return heroChoices;
+    }
+
+    // * scrapes name, sample size, winrate from a generic table on hotslogs
+    async scrapeGenericTable(minSampleSize : number = 30, page : Page) : Promise<Array<Hero>>
+    {
+        let heroStats = [];
+
+        let currentID = 0;
+        let stopSearching = false;
+
+        // todo export some of these to their separate functions
+
+        // todo we really should get the amount of heroes from somewhere and make this a for loop
+        // * loop over all the rows
+        while (!stopSearching) {
+            let rowElement = await page.$(`#__${currentID}`);
+
+            if (rowElement === null) {
+                stopSearching = true;
+                console.log("Broke out of the search loop at id ", currentID);
+                break;
+            }
+            let rowChildren = await rowElement.$$("td");
+
+            // * get sample size
+            let sampleSize = await rowChildren[2].evaluate(
+                (el: Element) => el.textContent
+            );
+
+            if (parseInt(sampleSize) >= minSampleSize) {
+                // * scrape all the hero data we can get (name, winrate (RELATIVE TO MAP), sample)
+
+                // * get hero name
+                let heroName = await rowChildren[1].evaluate(
+                    (el: Element) => el.textContent
+                );
+                console.log("Scraping hero " + heroName);
+
+                // * win %
+                let winRate = await rowChildren[rowChildren.length - 1].evaluate(
+                    (el: Element) => el.textContent
+                );
+
+                heroStats.push(
+                    new Hero(
+                        heroName,
+                        parseInt(sampleSize),
+                        parseFloat(winRate)
+                    )
+                );
+
+                // ? hero portrait url ?
+            }
+            currentID++;
+        }
+
+
+        return (heroStats);
     }
 
     async getHeroChoicesForMap(
