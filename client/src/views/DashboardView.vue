acf157c52f81363f4158e1cb9c287fa7c63cb99d
Gabriel <gabriel.griffin@epitech.eu>
2022-03-18 16:00:34 +0100

    client - U/C: began painfully designing the front end

--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -1,22 +1,44 @@
+<style>
+
+</style>
+
 <template>
-    <div>
-        <div>
-            Howdy
+    <div class="text-center">
+        <div class="pt-10">
+
+            <!-- <input
+                class="
+                    duration-100
+                    focus:border-b-2
+                    focus:border-cyan-300
+                    focus:outline-none
+                    rounded
+                   "
+                placeholder="Search for a map"> -->
+                <!-- // * cf https://headlessui.dev/vue/combobox -->
+            <Combobox>
+                <ComboboxInput />
+                <ComboboxOptions>
+                    <ComboboxOption>
+                    </ComboboxOption>
+                </ComboboxOptions>
+            </Combobox>
         </div>
-        <div class="underline">
-            Styled howdy
-        </div>
-        <HelloWorldButtonVue/>
     </div>
 </template>
 
 <script lang="ts">
     import { defineComponent } from "vue";
     import HelloWorldButtonVue from "../components/HelloWorldButton.vue";
+    import { Combobox, ComboboxInput, ComboboxOptions, ComboboxOption } from "@headlessui/vue";
 
     export default defineComponent({
         components: {
-            HelloWorldButtonVue
+            HelloWorldButtonVue,
+            Combobox,
+            ComboboxInput,
+            ComboboxOptions,
+            ComboboxOption
         }
     })
 </script>
