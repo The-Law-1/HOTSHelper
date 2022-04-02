36cf3bd8cd25245505ca78f71d654b3fbaf7aeb1
Gabriel <gabriel.griffin@epitech.eu>
2022-04-02 19:26:21 +0200

    client - U/C: found out you need to add host: true to server to run vite in a container

--- client/src/api/cats.ts
+++ client/src/api/cats.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-let serverHost = import.meta.env.VITE_SERVER_HOST || "http://localhost:";
+let serverHost = import.meta.env.VITE_SERVER_HOST || "http://localhost";
 let serverPort = import.meta.env.VITE_SERVER_PORT || 3001;
 
 const catsAPI = axios.create({
-    baseURL: `${serverHost}${serverPort}/cats`
+    baseURL: `${serverHost}:${serverPort}/cats`
 });
 
 export const getCats = async () => await catsAPI.get(`/`);
--- client/src/api/heroes.ts
+++ client/src/api/heroes.ts
@@ -1,11 +1,11 @@
 import axios, {AxiosResponse} from 'axios';
 import { Hero } from '../entities/hero';
 
-let serverHost = import.meta.env.VITE_SERVER_HOST || "http://localhost:";
+let serverHost = import.meta.env.VITE_SERVER_HOST || "http://localhost";
 let serverPort = import.meta.env.VITE_SERVER_PORT || 3001;
 
 const heroesAPI = axios.create({
-    baseURL: `${serverHost}${serverPort}/heroes`
+    baseURL: `${serverHost}:${serverPort}/heroes`
 });
 
 export const getHeroesForMap = async (queryParams : any) => await heroesAPI.get(`/mapwinrates/`, {params: queryParams, timeout: 20000});
